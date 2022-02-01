db4ee55dad25cd481875ec5d5ff1c2a0dd0020ed
Aykut Kardaş <aykutkrds@gmail.com>
2022-02-01 21:39:24 +0300

    Add `parse-markdowns` script with samples

---
+++ markdown/javascript.md
@@ -0,0 +1,10 @@
+# JavaScript
+
+This section is dedicated to the JavaScript language itself, and not the parts that are specific to Web pages or other host environments. For information about API specifics to Web pages, please see Web APIs and DOM.
+
+### Contributors
+
+- [Ebru Güleç](https://github.com/ebrugule/)
+- [Aykut Kardaş](https://github.com/aykutkardas)
+
+<!--- Tags: [javascript,es6] --->
---
+++ parse_markdowns.js
@@ -0,0 +1,35 @@
+const glob = require("glob");
+const fs = require("fs");
+const slugify = require("slugify");
+
+glob("markdown/*.md", function (err, files) {
+  const markdowns = [];
+
+  if (err) throw (err);
+
+  files.forEach((file) => {
+    const fileContent = fs.readFileSync(file, { encoding: "utf-8" });
+
+    const [titleLine, description] = fileContent.split("\n").filter((i) => i);
+    const [, title] = titleLine.match(/#\s([A-Za-z0-9- ]+)/);
+    const slug = slugify(title, { lower: true });
+    const tagsResult = fileContent.match(/<!--- Tags: \[([a-z0-9, ]+)\] --->/)
+
+    let tags = [];
+
+    if (tagsResult) {
+      tags = tagsResult[1].split(',').map(i => i.trim()).filter(i => i);
+    }
+
+    markdowns.push({
+      slug,
+      title,
+      description,
+      tags
+    });
+  });
+
+  fs.writeFileSync("data/markdowns.json", JSON.stringify(markdowns, null, 2), {
+    encoding: "utf-8",
+  });
+});
