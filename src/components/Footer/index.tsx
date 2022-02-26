99cf201e17218c8d1ec3299530a5ec3a0e940e2c
ebrugulec <gulecebrueg@gmail.com>
2022-02-27 02:55:46 +0300

    Add footer and icon component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import Icon from "../Icon";
+
+import styles from "./Footer.module.scss";
+
+const Footer = () => (
+  <footer data-testid="footer" className={styles.footer}>
+    <div className={styles.socialLinks}>
+      <a
+        href="https://github.com/ebrugulec/dev-cheat-sheets"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="GitHub"
+      >
+        <Icon icon="github" size={20} color="black" />
+      </a>
+    </div>
+    <div className="content">
+      Awesome Cheat Sheets
+    </div>
+  </footer>
+);
+  
+export default Footer;
+  
