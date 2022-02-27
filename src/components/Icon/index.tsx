e248ab44447ac19bee8585f0a5e8b108c7c6a994
Ebru Gulec <gulecebrueg@gmail.com>
2022-02-27 08:24:34 +0300

    Footer implementation (#18)

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,9 @@
+import IcoMoon, { IconComponent } from "react-icomoon";
+
+const iconSet = require("./selection.json");
+
+const Icon: typeof IconComponent = (props) => (
+  <IcoMoon iconSet={iconSet} {...props} />
+);
+
+export default Icon;
