82f3dba147612db1f843b37cc8b400f7e3a33b5f
Aykut Kardaş <aykutkrds@gmail.com>
2022-03-13 15:21:43 +0300

    Update import

--- src/hooks/UseDarkMode.tsx
+++ src/hooks/UseDarkMode.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export type DarkModeState = "dark" | "light";
 export type SetDarkModeState = React.Dispatch<
