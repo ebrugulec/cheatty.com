93c18d64bff15d36920bde09700fc89613bcb047
ebrugulec <gulecebrueg@gmail.com>
2022-03-19 17:44:06 +0300

    Add remark-gfm package and tag to markdown detail page

--- src/pages/cheatsheet/[slug].tsx
+++ src/pages/cheatsheet/[slug].tsx
@@ -1,35 +1,45 @@
 import type { GetServerSideProps } from "next";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import { MarkdownPropsWithContent } from "@components/Card";
 
 import styles from "./Cheatsheet.module.scss";
+import Tag from "@components/Tag";
 
-const CheatsheetDetail = ({ content, error }: MarkdownPropsWithContent) => {
+const CheatsheetDetail = ({ content, error, tags }: MarkdownPropsWithContent) => {
   return (
     <div className={styles.page}>
       {error ? (
         <div>{error}</div>
       ) : (
-        <ReactMarkdown
-          components={{
-            code({ inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter language={match[1]} PreTag="div" {...props}>
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
-        >
-          {content}
-        </ReactMarkdown>
+        <>
+          <div className={styles.tagList}>
+            {tags.map((tag) => (
+              <Tag key={tag} name={tag} />
+            ))}
+          </div>
+          <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || "");
+                  return !inline && match ? (
+                    <SyntaxHighlighter language={match[1]} PreTag="div" {...props}>
+                      {String(children).replace(/\n$/, "")}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            >
+              {content}
+            </ReactMarkdown>
+        </>
       )}
     </div>
   );
